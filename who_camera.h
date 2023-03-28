6c975f32e292d6cdf7bb80e589666f764f49e5e6
Lovmong <1967471283@qq.com>
2023-03-28 16:23:04 +0800

    Reduce camera XCLK_FREQ to reduce heat generation

--- who_camera.h
+++ who_camera.h
@@ -6,7 +6,10 @@
 #include "freertos/semphr.h"
 #include "freertos/task.h"
 
-#define XCLK_FREQ_HZ 20000000
+// #define XCLK_FREQ_HZ 20000000
+#define XCLK_FREQ_HZ 15000000
+// #define XCLK_FREQ_HZ 12000000
+
 
 #ifdef __cplusplus
 extern "C" {
