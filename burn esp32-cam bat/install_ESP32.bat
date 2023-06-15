b82af9adc4a188ad13f9d414482d6b6ade87b782
Lovmong <1967471283@qq.com>
2023-06-15 18:19:37 +0800

    add baud param

--- burn esp32-cam bat/install_ESP32.bat
+++ burn esp32-cam bat/install_ESP32.bat
@@ -1,5 +1,6 @@
 @REM esptool.exe -help
 
+set baud=921600
 set boot_app0="boot_app0.bin"
 set bootloader="ai-camera-firmware_v1.2.0_bootloader.bin"
 set partitions="ai-camera-firmware_v1.2.0_partitions.bin"
@@ -14,7 +15,7 @@ set merge_app="ai-camera-firmware_v1.2.0_merged.bin"
 @REM esptool.exe --chip esp32 --before default_reset erase_flash
 @REM esptool.exe --chip esp32 --before default_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 %bootloader% 0x8000 %partitions% 0xe000 %boot_app0% 0x10000 %app% 
 
-esptool.exe --chip esp32 --before default_reset erase_flash
-esptool.exe --chip esp32 --before default_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 %merge_app%
+esptool.exe --chip esp32 --baud %baud% --before default_reset erase_flash
+esptool.exe --chip esp32 --baud %baud% --before default_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 %merge_app%
 
 pause
