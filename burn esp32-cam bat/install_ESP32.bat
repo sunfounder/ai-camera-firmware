92a0da637128bfe382c7e1c7c6adb31411b146cc
Cavon <cavonxx@gmail.com>
2025-01-03 11:20:32 +0800

    Remove Settings title

--- burn esp32-cam bat/install_ESP32.bat
+++ burn esp32-cam bat/install_ESP32.bat
@@ -1,19 +1,9 @@
 @REM esptool.exe -help
 
 set baud=921600
-set boot_app0="boot_app0.bin"
-set bootloader="ai-camera-firmware-v1.4.0-bootloader.bin"
-set partitions="ai-camera-firmware-v1.4.0-partitions.bin"
-set app="ai-camera-firmware-v1.4.0.bin"
-set merge_app="ai-camera-firmware-v1.4.0-merged.bin"
-
-@REM esptool.exe --chip esp32 --before no_reset_no_sync erase_flash
-@REM esptool.exe --chip esp32 --before no_reset_no_sync write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 %bootloader% 0x8000 %partitions% 0xe000 %boot_app0% 0x10000 %firmware% 
-
-@REM esptool.exe --chip esp32 --before default_reset erase_flash
-@REM esptool.exe --chip esp32 --before default_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 %bootloader% 0x8000 %partitions% 0xe000 %boot_app0% 0x10000 %app% 
+set bin_file="ai-camera-firmware-v1.4.1-factory.bin"
 
 esptool.exe --chip esp32 --baud %baud% --before default_reset erase_flash
-esptool.exe --chip esp32 --baud %baud% --before default_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 %merge_app%
+esptool.exe --chip esp32 --baud %baud% --before default_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 %bin_file%
 
 pause
