181f63d0a0ca81e31aa4ab24591b6ebdeaae32d8
Cavon <cavonxx@gmail.com>
2025-08-22 18:09:55 +0800

    feat(esp32-cam): update firmware to v1.4.2 and flash script
    Update the installation script to support new firmware version v1.4.2 with separate bootloader, partitions, and app binaries. Remove old v1.4.1 firmware files.

--- burn esp32-cam bat/install_ESP32.bat
+++ burn esp32-cam bat/install_ESP32.bat
@@ -1,9 +1,13 @@
 @REM esptool.exe -help
 
 set baud=921600
-set bin_file="ai-camera-firmware-v1.4.1-factory.bin"
+set boot_app0="boot_app0.bin"
+set bootloader="ai-camera-firmware-v1.4.2-bootloader.bin"
+set partitions="ai-camera-firmware-v1.4.2-partitions.bin"
+set bin_file="ai-camera-firmware-v1.4.2-ota.bin"
 
 esptool.exe --chip esp32 --baud %baud% --before default_reset erase_flash
-esptool.exe --chip esp32 --baud %baud% --before default_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x0000 %bin_file%
+esptool.exe --chip esp32 --baud %baud% --before default_reset write_flash -z 
+--flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 %bootloader% 0x8000 %partitions% 0xe000 %boot_app0% 0x10000 %bin_file%
 
 pause
